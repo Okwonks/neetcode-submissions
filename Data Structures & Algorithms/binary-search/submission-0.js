d17e0fd6457722fdfa3ecd815dd0f649c7b8d617
Albert Oketch <albertoketch@gmail.com>
2026-09-01 08:20:44 +0300

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0, right = nums.length - 1;
+        for(; left <= right; ) {
+            const mid = left + Math.floor((right - left) / 2);
+            if(nums[mid] > target) {
+                right = mid - 1;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else if(nums[mid] === target) {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
